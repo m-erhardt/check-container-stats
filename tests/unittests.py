3c53ca39fe488672d8d253da39db2ef3e8efb5dc
Mauno Erhardt <maunoerhardt@web.de>
2024-11-16 13:52:28 +0100

    Multiple changes, see full commit message
    - Removed unit test for removed function
    - Configured pylint for max-line-lenght=120
    - Bumped GitHub actions
    - Moved img folder to docs

--- tests/unittests.py
+++ tests/unittests.py
@@ -12,14 +12,6 @@ import check_container_stats_podman
 
 class TestMetricDetection(unittest.TestCase):
 
-    def test_docker_metric_unit_detection(self):
-        # Check check_container_stats_docker.py for different metric notations
-        self.assertEqual(check_container_stats_docker.convert_to_bytes('105 MB'), 105000000)
-        self.assertEqual(check_container_stats_docker.convert_to_bytes('105TiB'), 115448720916480)
-        self.assertEqual(check_container_stats_docker.convert_to_bytes('512 B'), 512)
-        self.assertEqual(check_container_stats_docker.convert_to_bytes('105e+3MB'), 105000000000)
-
-
     def test_podman_metric_unit_detection(self):
         # Check check_container_stats_podman.py for different metric notations
         self.assertEqual(check_container_stats_podman.convert_to_bytes('105 MB'), 105000000)
