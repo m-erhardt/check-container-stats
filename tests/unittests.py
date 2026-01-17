b7aec7aa9700132a3743ec7bb1eccde6a209fa4c
Mauno Erhardt <maunoerhardt@web.de>
2026-01-17 15:37:56 +0100

    Add unit tests

--- tests/unittests.py
+++ tests/unittests.py
@@ -2,12 +2,49 @@
 
 import sys
 import unittest
+import unittest.mock
 
 sys.path.append('..')
 sys.path.append('.')
 
 import check_container_stats_docker
 import check_container_stats_podman
+import check_docker_system
+
+class TestScriptExecution(unittest.TestCase):
+    """
+    Test if main() function of non-async plugins executes without exception and performs clean exit
+    """
+
+    def test_check_container_stats_docker_help(self):
+        with unittest.mock.patch('sys.argv', ['check_container_stats_docker.py', '--help']):
+            with self.assertRaises(SystemExit) as exit_code:
+                check_container_stats_docker.main()
+
+            # Test if exit code was 0
+            self.assertEqual(exit_code.exception.code, 0)
+
+    def test_check_container_stats_podman_help(self):
+        with unittest.mock.patch('sys.argv', ['check_container_stats_podman.py', '--help']):
+            with self.assertRaises(SystemExit) as exit_code:
+                check_container_stats_podman.main()
+
+            # Test if exit code was 0
+            self.assertEqual(exit_code.exception.code, 0)
+
+
+class TestAsyncScriptExecution(unittest.IsolatedAsyncioTestCase):
+    """
+    Test if main() function of async plugins executes without exception and performs clean exit
+    """
+
+    async def test_check_docker_system_help(self):
+        with unittest.mock.patch('sys.argv', ['check_docker_system.py', '--help']):
+            with self.assertRaises(SystemExit) as exit_code:
+                await check_docker_system.main()
+
+            # Test if exit code was 0
+            self.assertEqual(exit_code.exception.code, 0)
 
 
 class TestMetricDetection(unittest.TestCase):
